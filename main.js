9da4662156d49b9259c70e368c1694a9af2acce4
Ibrohim <ibrohimmull@gmail.com>
2026-01-09 23:04:20 +0500

    homework

---
+++ main.js
@@ -0,0 +1,22 @@
+
+let users = {};
+
+for (let i = 1; i <= 10; i++) {
+  let name = prompt(`Введите имя пользователя ${i}`);
+  let age = prompt(`Введите возраст пользователя ${i}`);
+
+  
+  users[i] = {
+    name: name,
+    age: age
+  };
+}
+
+
+for (let key in users) {
+  console.log(`Пользователь - ${key}`);
+  console.log(`Имя - ${users[key].name}`);
+  console.log(`Возраст - ${users[key].age}`);
+}
+
+console.log(users);
